9a68a0e8f147e7f2a6cb0b041cc9c8a02b084955
Hammer Master <chai5142xshot@gmail.com>
2018-05-14 17:40:51 +0700

    Update server.lua
    Check for long play solo session.

--- solokick/server.lua
+++ solokick/server.lua
@@ -2,7 +2,10 @@ RegisterServerEvent('sendSessionPlayerNumber')
 AddEventHandler('sendSessionPlayerNumber', function(clientPlayerNumber)
 	if source ~= nil then
 		serverPlayerNumber = countPlayer()
-		if clientPlayerNumber ~= serverPlayerNumber and clientPlayerNumber == 1 then
+		if (clientPlayerNumber ~= serverPlayerNumber) and (clientPlayerNumber == 1) then -- For first spawn solo
+			DropPlayer(source, '[Kick] Solo session.') -- kick player
+			print("Solo session clientPlayerNumber-"..clientPlayerNumber.." serverPlayerNumber-"..serverPlayerNumber) -- debug
+		elseif (serverPlayerNumber-5 >= 1) and (clientPlayerNumber < serverPlayerNumber-5) then -- For long play solo
 			DropPlayer(source, '[Kick] Solo session.') -- kick player
 			print("Solo session clientPlayerNumber-"..clientPlayerNumber.." serverPlayerNumber-"..serverPlayerNumber) -- debug
 		end
@@ -16,21 +19,3 @@ function countPlayer() -- count all players
 	end
 	return counter
 end
-
--- Version Checking down here, better don't touch this
-local CurrentVersion = '2.0'
-PerformHttpRequest('https://raw.githubusercontent.com/chaixshot/fivem/master/solokick/version', function(Error, NewestVersion, Header)
-	if CurrentVersion ~= NewestVersion then
-		print('\n')
-		print('##')
-		print('## Solo Kick')
-		print('##')
-		print('## Current Version: ' .. CurrentVersion)
-		print('## Newest Version: ' .. NewestVersion)
-		print('##')
-		print('## Download')
-		print('## https://github.com/chaixshot/fivem/tree/master/solokick')
-		print('##')
-		print('\n')
-	end
-end)
