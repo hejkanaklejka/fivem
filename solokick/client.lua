acf28c02a7e734303df305a26a0779282f2b0083
Hammer Master <chai5142xshot@gmail.com>
2018-05-24 16:44:34 +0700

    Update client.lua

--- solokick/client.lua
+++ solokick/client.lua
@@ -1,27 +1,24 @@
 local state_ready = false
+local playerconnecting = false
 
 AddEventHandler("playerSpawned",function() -- delay state recording
-	SetTimeout(30000, function()
-		state_ready = true
-	end)
+	Citizen.Wait(30000)
+	state_ready = true
 end)
 
-RegisterNetEvent("sendSession:CheckSoloPlayer")
-AddEventHandler("sendSession:CheckSoloPlayer", function(timer)
-	if state_ready then
-		TriggerServerEvent('sendSession:PlayerNumber', GetNumberOfPlayers())
-		print("sendSession:PlayerNumber")
-	end
+RegisterNetEvent("sendSession:PlayerConnecting")
+AddEventHandler("sendSession:PlayerConnecting", function()
+	playerconnecting = true
+	Citizen.Wait(120000)
+	playerconnecting= false
 end)
 
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(0)
-		if NetworkIsSessionStarted() then
-			Citizen.Wait(10000)
-			TriggerServerEvent( "sendSession:firstJoin")
-			print("sendSession:firstJoin")
-			return
+		if state_ready and not playerconnecting then
+			TriggerServerEvent('sendSession:PlayerNumber', GetNumberOfPlayers())
+			Wait(60000)
 		end
+		Wait(0)
 	end
 end)
