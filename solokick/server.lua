58cffc19ec5d900a8c7ecaf12167b5c8024bb798
Hammer Master <chai5142xshot@gmail.com>
2018-05-24 17:46:19 +0700

    Update server.lua

--- solokick/server.lua
+++ solokick/server.lua
@@ -1,18 +1,5 @@
-RegisterServerEvent('sendSession:PlayerNumber')
-AddEventHandler('sendSession:PlayerNumber', function(clientPlayerNumber)
-	if source ~= nil then
-		serverPlayerNumber = countPlayer()
-		if (clientPlayerNumber ~= serverPlayerNumber) and (clientPlayerNumber == 1) then -- For first spawn solo
-			DropPlayer(source, '[Kick] Solo session.') -- kick player
-			print("Solo session clientPlayerNumber-"..clientPlayerNumber.." serverPlayerNumber-"..serverPlayerNumber) -- debug
-		elseif (serverPlayerNumber-5 >= 1) and (clientPlayerNumber < serverPlayerNumber-5) then -- For long play solo
-			DropPlayer(source, '[Kick] Solo session.') -- kick player
-			print("Solo session clientPlayerNumber-"..clientPlayerNumber.." serverPlayerNumber-"..serverPlayerNumber) -- debug
-		end
-	end
-end)
-
-function countPlayer() -- count all players
+local PlayerConnecting = {}
+local function countPlayer() -- count all players
 	local counter = 0
 	for _ in pairs(GetPlayers()) do
 		counter = counter + 1
@@ -20,18 +7,43 @@ function countPlayer() -- count all players
 	return counter
 end
 
+RegisterServerEvent('sendSession:PlayerNumber')
+AddEventHandler('sendSession:PlayerNumber', function(clientPlayerNumber)
+	if source ~= nil then
+		serverPlayerNumber = countPlayer()
+		if (clientPlayerNumber ~= serverPlayerNumber) and (clientPlayerNumber == 1) then -- For first spawn solo
+			DropPlayer(source, '[Kick] Solo session.') -- kick player
+			print("sendSession:PlayerNumber clientPlayerNumber-"..clientPlayerNumber.." serverPlayerNumber-"..serverPlayerNumber) -- debug
+		elseif (serverPlayerNumber-5 >= 1) and (clientPlayerNumber < serverPlayerNumber-5) then -- For long play solo
+			DropPlayer(source, '[Kick] Solo session.') -- kick player
+			print("sendSession:PlayerNumber clientPlayerNumber-"..clientPlayerNumber.." serverPlayerNumber-"..serverPlayerNumber) -- debug
+		end
+	end
+end)
+
 AddEventHandler("playerConnecting",function(name,setMessage) -- Fix player connecting
-	TriggerClientEvent('sendSession:PlayerConnecting', -1)
+	TriggerClientEvent('sendSession:PlayerConnecting', -1, source)
+	PlayerConnecting[source]=source
 end)
 
 RegisterServerEvent('sendSession:PlayerSpawned') -- Fix player connecting
 AddEventHandler('sendSession:PlayerSpawned', function()
-	TriggerClientEvent('sendSession:PlayerSpawned', -1)
+	TriggerClientEvent('sendSession:PlayerSpawned', -1, source)
+	PlayerConnecting[source]=nil
+end)
+
+AddEventHandler("playerDropped",function() -- Fix player connecting
+	if PlayerConnecting[source] ~= nil then
+		TriggerClientEvent('sendSession:PlayerSpawned', -1, source)
+		PlayerConnecting[source]=nil
+	end
 end)
 
 
+
+
 -- Check for update
-local CurrentVersion = [[3.0
+local CurrentVersion = [[3.1
 ]]
 PerformHttpRequest('https://raw.githubusercontent.com/chaixshot/fivem/master/solokick/version', function(Error, NewestVersion, Header)
 	if CurrentVersion ~= NewestVersion then
