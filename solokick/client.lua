a4dfa248446688fa354f9438ef0142028483759b
Hammer Master <chai5142xshot@gmail.com>
2018-05-25 15:55:35 +0700

    Update client.lua

--- solokick/client.lua
+++ solokick/client.lua
@@ -1,41 +1,36 @@
-local state_ready = false
 local playerconnecting = 0
 local PlayerConnectingId = {}
 
-AddEventHandler("playerSpawned",function() -- Delay client state
-	Citizen.Wait(30000)
-	state_ready = true
-end)
-
 RegisterNetEvent("sendSession:PlayerConnecting") -- Call when other player connecting
-AddEventHandler("sendSession:PlayerConnecting", function(PlayerId)
-	if PlayerConnectingId[PlayerId] == nil then
+AddEventHandler("sendSession:PlayerConnecting", function(PlayerName)
+	if PlayerConnectingId[PlayerName] == nil then
 		playerconnecting = playerconnecting+1
-		PlayerConnectingId[PlayerId] = 1
-		print("sendSession:playerconnecting: "..playerconnecting)
+		PlayerConnectingId[PlayerName] = 1
+		print("Solo Player Connecting: "..playerconnecting) -- Debug
 	end
 end)
 
 RegisterNetEvent("sendSession:PlayerSpawned") -- Call when other player connected
-AddEventHandler("sendSession:PlayerSpawned", function(PlayerId)
-	if PlayerConnectingId[PlayerId] ~= nil then
+AddEventHandler("sendSession:PlayerSpawned", function(PlayerName)
+	if PlayerConnectingId[PlayerName] ~= nil then
 		playerconnecting = playerconnecting-1
-		PlayerConnectingId[PlayerId] = nil
-		print("sendSession:playerconnecting: "..playerconnecting)
+		PlayerConnectingId[PlayerName] = nil
+		print("Solo Player Connecting: "..playerconnecting) -- Debug
 	end
 end)
 
-Citizen.CreateThread(function() -- Check solo session every 1 minute
+Citizen.CreateThread(function() -- Check solo session every 30 sec
+	Wait(30000) -- Delay first spawn.
 	while true do
-		if state_ready and playerconnecting == 0 then
+		if playerconnecting == 0 then
 			TriggerServerEvent('sendSession:PlayerNumber', GetNumberOfPlayers())
-			print("sendSession:PlayerNumber") -- Debug
-			Wait(60000)
+			print("Solo Checking") -- Debug
+			Wait(30000)
 		end
 		Wait(0)
 	end
 end)
 
-AddEventHandler("playerSpawned",function() -- delay state recording
+AddEventHandler("playerSpawned",function() -- Send when other spawned.
 	TriggerServerEvent("sendSession:PlayerSpawned")
 end)
