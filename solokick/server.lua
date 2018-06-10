d2f9e947e71ace505f27301e44c7a3c90865864f
Hammer Master <chai5142xshot@gmail.com>
2018-06-10 09:33:23 +0700

    Update server.lua

--- solokick/server.lua
+++ solokick/server.lua
@@ -10,35 +10,16 @@ RegisterServerEvent('sendSession:PlayerNumber')
 AddEventHandler('sendSession:PlayerNumber', function(clientPlayerNumber)
 	if source ~= nil then
 		serverPlayerNumber = countPlayer()
-		if serverPlayerNumber-clientPlayerNumber > 2 then 
+		if serverPlayerNumber-clientPlayerNumber > 4 then 
 			DropPlayer(source, '[Kick] Solo session.') -- Kick player
 			print("sendSession:PlayerNumber clientPlayerNumber-"..clientPlayerNumber.." serverPlayerNumber-"..serverPlayerNumber) -- Debug
 		end
 	end
 end)
 
-AddEventHandler( 'playerConnecting', function( name, setReason )  -- Fix player connecting
-	-- 'source' now working on playerConnecting event.
-	TriggerClientEvent('sendSession:PlayerConnecting', -1, name)
-	print("------Solo PlayerConnecting "..name.."------") -- Debug
-end)
-
-RegisterServerEvent('sendSession:PlayerSpawned') -- Fix player connecting
-AddEventHandler('sendSession:PlayerSpawned', function()
-	TriggerClientEvent('sendSession:PlayerSpawned', -1, GetPlayerName(source))
-	print("------Solo PlayerSpawned "..GetPlayerName(source).."------") -- Debug
-end)
-
-AddEventHandler("playerDropped",function(reason) -- Fix player connecting
-	TriggerClientEvent('sendSession:PlayerSpawned', -1, GetPlayerName(source))
-	print("------Solo PlayerDroped "..GetPlayerName(source).."------") -- Debug
-end)
-
-
-
 
 -- Check for update
-local CurrentVersion = [[3.4
+local CurrentVersion = [[3.5
 ]]
 PerformHttpRequest('https://raw.githubusercontent.com/chaixshot/fivem/master/solokick/version', function(Error, NewestVersion, Header)
 	if CurrentVersion ~= NewestVersion then
